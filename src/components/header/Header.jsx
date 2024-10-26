39b82ad4a88141b6ebb050d3981a7c24e3f7ec3f
katrinsukovati <katrin.sukovati1@gmail.com>
2024-10-26 16:53:18 -0400

    header structure

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.scss";
+import brainFlixLogo from "../../assets/logo/BrainFlix-logo.svg";
+
+function Header() {
+  return (
+    <div className="header">
+      <img className="header__logo" src={brainFlixLogo} alt="BrainFlix Logo" />
+      <div className="header__options">
+        <input className="" type="text" placeholder="Search" />
+        <button className="header__upload-button">UPLOAD</button>
+        <div className="header__user"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
