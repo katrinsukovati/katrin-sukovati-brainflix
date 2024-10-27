b7dc2cabc14bd305365a77935b888dc0d10fff58
katrinsukovati <katrin.sukovati1@gmail.com>
2024-10-27 15:49:48 -0400

    video details section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Header from "./components/header/Header";
 import videoData from "./data/video-details.json";
 import Video from "./components/video/Video";
+import VideoDetails from "./components/videoDetails/VideoDetails";
 
 function App() {
   const [video, setVideo] = useState(videoData[0]);
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <Video video={video} />
+      <VideoDetails video={video} />
     </div>
   );
 }
--- src/components/videoDetails/VideoDetails.jsx
+++ src/components/videoDetails/VideoDetails.jsx
@@ -1,5 +1,49 @@
-function VideoDetails() {
-  return <div></div>;
+import viewsIcon from "../../assets/icons/views.svg";
+import likesIcon from "../../assets/icons/likes.svg";
+import "./VideoDetails.scss";
+
+function VideoDetails({ video }) {
+  return (
+    <div className="video-details">
+      <h1 className="video-details__header">{video.title}</h1>
+      <div className="video-details__info">
+        <div className="video-details__left-section">
+          <p className="video-details__channel">By {video.channel}</p>
+          <p className="video-details__day">
+            {new Date(video.timestamp).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "numeric",
+              day: "numeric",
+            })}
+          </p>
+        </div>
+        <div className="video-details__right-section">
+          <div className="video-details__views-section">
+            <img
+              className="video-details__views-icon"
+              src={viewsIcon}
+              alt="views icon"
+            />
+            <p className="video-details__views">{video.views}</p>
+          </div>
+          <div className="video-details__likes-section">
+            <img
+              className="video-details__likes-icon"
+              src={likesIcon}
+              alt="likes icon"
+            />
+            <p className="video-details__likes">{video.likes}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="video-details__desc">{video.description}</p>
+      </div>
+      <p className="video-details__comments">
+        {video.comments.length} Comments
+      </p>
+    </div>
+  );
 }
 
 export default VideoDetails;
